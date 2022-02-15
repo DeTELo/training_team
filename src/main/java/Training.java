72f0165d6d408f4787c071620dd60efe004b6758
Dmitrijs <admin@gmail.com>
2022-02-15 20:26:49 +0200

    new commit 34

--- src/main/java/Training.java
+++ src/main/java/Training.java
@@ -1,6 +1,6 @@
 public class Training {
 
     public static void main(String[] args) {
-        System.out.println("Hello in Training Team , You are Welcome!!!");
+        System.out.println("Hello in Training Team , You are Welcome!!!!");
     }
 }
